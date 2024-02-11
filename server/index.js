c1d9200cbde05fe82e36db9a219a224eed972621
ericpkatz <ericpkatz@gmail.com>
2024-02-11 08:56:19 -0500

    deployable

--- server/index.js
+++ server/index.js
@@ -15,6 +15,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+//for deployment only
+const path = require('path');
+app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html')));
+app.use('/assets', express.static(path.join(__dirname, '../client/dist/assets'))); 
+
+
 app.post('/api/auth/login', async(req, res, next)=> {
   try {
     res.send(await authenticate(req.body));
