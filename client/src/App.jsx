900fb0306ccef15435a13c6cf25b729ea3fb0418
wareez92 <wareezsowemimo@gmail.com>
2024-07-09 20:32:48 -0500

    final touches

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,59 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-const Login = ({ login })=> {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ login }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const submit = ev => {
+  const submit = (ev) => {
     ev.preventDefault();
     login({ username, password });
-  }
+  };
   return (
-    <form onSubmit={ submit }>
-      <input value={ username } placeholder='username' onChange={ ev=> setUsername(ev.target.value)}/>
-      <input value={ password} placeholder='password' onChange={ ev=> setPassword(ev.target.value)}/>
-      <button disabled={ !username || !password }>Login</button>
+    <form onSubmit={submit}>
+      <input
+        value={username}
+        placeholder="username"
+        onChange={(ev) => setUsername(ev.target.value)}
+      />
+      <input
+        value={password}
+        placeholder="password"
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button disabled={!username || !password}>Login</button>
     </form>
   );
-}
+};
 
 function App() {
   const [auth, setAuth] = useState({});
   const [products, setProducts] = useState([]);
   const [favorites, setFavorites] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     attemptLoginWithToken();
   }, []);
 
-  const attemptLoginWithToken = async()=> {
-    const token = window.localStorage.getItem('token');
-    if(token){
-      const response = await fetch(`/api/auth/me`, {
+  const attemptLoginWithToken = async () => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      const response = await fetch("/api/auth/me", {
         headers: {
           authorization: token
-        }
+        },
       });
       const json = await response.json();
-      if(response.ok){
+      if (response.ok) {
         setAuth(json);
-      }
-      else {
-        window.localStorage.removeItem('token');
+      } else {
+        window.localStorage.removeItem("token");
       }
     }
   };
 
-  useEffect(()=> {
-    const fetchProducts = async()=> {
-      const response = await fetch('/api/products');
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("/api/products");
       const json = await response.json();
       setProducts(json);
     };
@@ -54,102 +61,106 @@ function App() {
     fetchProducts();
   }, []);
 
-  useEffect(()=> {
-    const fetchFavorites = async()=> {
-      const response = await fetch(`/api/users/${auth.id}/favorites`);
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      const response = await fetch(`/api/users/${auth.id}/favorites`, {
+        headers: {
+          authorization: window.localStorage.getItem("token"),
+        },
+      });
       const json = await response.json();
-      if(response.ok){
+      if (response.ok) {
         setFavorites(json);
       }
     };
-    if(auth.id){
+    if (auth.id) {
       fetchFavorites();
-    }
-    else {
+    } else {
       setFavorites([]);
     }
   }, [auth]);
 
-  const login = async(credentials)=> {
-    const response = await fetch('/api/auth/login', {
-      method: 'POST',
+  const login = async (credentials) => {
+    const response = await fetch("/api/auth/login", {
+      method: "POST",
       body: JSON.stringify(credentials),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       }
     });
 
     const json = await response.json();
-    if(response.ok){
-      window.localStorage.setItem('token', json.token);
+    if (response.ok) {
+      window.localStorage.setItem("token", json.token);
       attemptLoginWithToken();
     }
-    else {
+  };
+
+  const addFavorite = async (product_id) => {
+    const response = await fetch(`/api/users/${auth.id}/favorites`, {
+      method: "POST",
+      body: JSON.stringify({ product_id }),
+      headers: {
+        "Content-Type": "application/json",
+        authorization: window.localStorage.getItem("token"),
+      },
+    });
+
+    const json = await response.json();
+    if (response.ok) {
+      setFavorites([...favorites, json]);
+    } else {
       console.log(json);
     }
   };
 
-  const addFavorite = async(product_id)=> {
-    const response = await fetch(`/api/users/${auth.id}/favorites`, {
-      method: 'POST',
-      body: JSON.stringify({ product_id }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const json = await response.json();
-    if(response.ok){
-      setFavorites([...favorites, json]);
-    }
-    else {
-      console.log(json);
-    }
-  }
-
-  const removeFavorite = async(id)=> {
+  const removeFavorite = async (id) => {
     const response = await fetch(`/api/users/${auth.id}/favorites/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
+      headers: {
+        authorization: window.localStorage.getItem("token"),
+      },
     });
 
-    if(response.ok){
-      setFavorites(favorites.filter(favorite => favorite.id !== id));
-    }
-    else {
+    if (response.ok) {
+      setFavorites(favorites.filter((favorite) => favorite.id !== id));
+    } else {
       console.log(json);
     }
-  }
+  };
 
-  const logout = ()=> {
-    window.localStorage.removeItem('token');
+  const logout = () => {
+    window.localStorage.removeItem("token");
     setAuth({});
   };
 
   return (
     <>
-      {
-        !auth.id ? <Login login={ login }/> : <button onClick={ logout }>Logout { auth.username }</button>
-      }
+      {!auth.id ? (
+        <Login login={login} />
+      ) : (
+        <button onClick={logout}>Logout {auth.username}</button>
+      )}
       <ul>
-        {
-          products.map( product => {
-            const isFavorite = favorites.find(favorite => favorite.product_id === product.id);
-            return (
-              <li key={ product.id } className={ isFavorite ? 'favorite': ''}>
-                { product.name }
-                {
-                  auth.id && isFavorite && <button onClick={()=> removeFavorite(isFavorite.id)}>-</button>
-                }
-                {
-                  auth.id && !isFavorite && <button onClick={()=> addFavorite(product.id)}>+</button>
-                }
-              </li>
-            );
-          })
-        }
+        {products.map((product) => {
+          const isFavorite = favorites.find(
+            (favorite) => favorite.product_id === product.id
+          );
+          return (
+            <li key={product.id} className={isFavorite ? "favorite" : ""}>
+              {product.name}
+              {auth.id && isFavorite && (
+                <button onClick={() => removeFavorite(isFavorite.id)}>-</button>
+              )}
+              {auth.id && !isFavorite && (
+                <button onClick={() => addFavorite(product.id)}>+</button>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -3,120 +3,179 @@ const {
   createTables,
   createUser,
   createProduct,
-  createFavorite,
   fetchUsers,
   fetchProducts,
-  fetchFavorites,
+  createFavorite,
   destroyFavorite,
+  fetchFavorites,
   authenticate,
-  findUserWithToken
-} = require('./db');
-const express = require('express');
+  findUserWithToken,
+} = require("./db");
+
+
+const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const path = require("path");
 app.use(express.json());
 
-//for deployment only
-const path = require('path');
-app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../client/dist/index.html')));
-app.use('/assets', express.static(path.join(__dirname, '../client/dist/assets'))); 
+const secret = process.env.JWT || "shhhhlocal";
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+);
+app.use(
+  "/assets",
+  express.static(path.join(__dirname, "../client/dist/assets"))
+);
 
-app.post('/api/auth/login', async(req, res, next)=> {
+const isLoggedIn = async (req, res, next) => {
   try {
-    res.send(await authenticate(req.body));
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      const error = Error("not authorized");
+      error.status = 401;
+      throw error;
+    }
+    req.user = await findUserWithToken(token);
+    next();
+  } catch (error) {
+    next(error);
   }
-  catch(ex){
+};
+
+app.post("/api/auth/login", async (req, res, next) => {
+  try {
+    const token = await authenticate(req.body);
+    res.send({ token });
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/auth/me', async(req, res, next)=> {
+app.post("/api/auth/register", async (req, res, next) => {
   try {
-    res.send(await findUserWithToken(req.headers.authorization));
-  }
-  catch(ex){
+    const user = await createUser(req.body);
+    const token = jwt.sign({ id: user.id }, secret);
+    res.send({ token });
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/users', async(req, res, next)=> {
+app.get("/api/auth/me", isLoggedIn, async (req, res, next) => {
+  try {
+    res.send(req.user);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/api/users", async (req, res, next) => {
   try {
     res.send(await fetchUsers());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/users/:id/favorites', async(req, res, next)=> {
+app.get("/api/users/:id/favorites", isLoggedIn, async (req, res, next) => {
   try {
-    res.send(await fetchFavorites(req.params.id));
-  }
-  catch(ex){
+    if (req.user.id === req.params.id) {
+      res.send(await fetchFavorites(req.params.id));
+    } else {
+      const error = Error("not authorized");
+      error.status = 401;
+      throw error;
+    }
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.post('/api/users/:id/favorites', async(req, res, next)=> {
+app.post("/api/users/:id/favorites", isLoggedIn, async (req, res, next) => {
   try {
-    res.status(201).send(await createFavorite({ user_id: req.params.id, product_id: req.body.product_id}));
-  }
-  catch(ex){
+    if (req.user.id === req.params.id) {
+      res.status(201).send(
+        await createFavorite({
+          user_id: req.params.id,
+          product_id: req.body.product_id,
+        })
+      );
+    } else {
+      const error = Error("not authorized");
+      error.status = 401;
+      throw error;
+    }
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.delete('/api/users/:user_id/favorites/:id', async(req, res, next)=> {
-  try {
-    await destroyFavorite({user_id: req.params.user_id, id: req.params.id });
-    res.sendStatus(204);
+app.delete(
+  "/api/users/:user_id/favorites/:id",
+  isLoggedIn,
+  async (req, res, next) => {
+    try {
+      if (req.user.id === req.params.user_id) {
+        await destroyFavorite({
+          user_id: req.params.user_id,
+          id: req.params.id,
+        });
+        res.sendStatus(204);
+      } else {
+        const error = Error("not authorized");
+        error.status = 401;
+        throw error;
+      }
+    } catch (ex) {
+      next(ex);
+    }
   }
-  catch(ex){
-    next(ex);
-  }
-});
+);
 
-app.get('/api/products', async(req, res, next)=> {
+app.get("/api/products", async (req, res, next) => {
   try {
     res.send(await fetchProducts());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.use((err, req, res, next)=> {
-  console.log(err);
-  res.status(err.status || 500).send({ error: err.message ? err.message : err });
+app.use((err, req, res, next) => {
+  console.error(err);
+  res
+    .status(err.status || 500)
+    .send({ error: err.message ? err.message : err });
 });
 
-const init = async()=> {
+const init = async () => {
   const port = process.env.PORT || 3000;
   await client.connect();
-  console.log('connected to database');
+  console.log("connected to database");
 
   await createTables();
-  console.log('tables created');
+  console.log("tables created");
 
-  const [moe, lucy, ethyl, curly, foo, bar, bazz, quq, fip] = await Promise.all([
-    createUser({ username: 'moe', password: 'm_pw'}),
-    createUser({ username: 'lucy', password: 'l_pw'}),
-    createUser({ username: 'ethyl', password: 'e_pw'}),
-    createUser({ username: 'curly', password: 'c_pw'}),
-    createProduct({ name: 'foo' }),
-    createProduct({ name: 'bar' }),
-    createProduct({ name: 'bazz' }),
-    createProduct({ name: 'quq' }),
-    createProduct({ name: 'fip' })
-  ]);
+  const [moe, lucy, ethyl, curly, foo, bar, bazz, quq, fip] = await Promise.all(
+    [
+      createUser({ username: "moe", password: "m_pw" }),
+      createUser({ username: "lucy", password: "l_pw" }),
+      createUser({ username: "ethyl", password: "e_pw" }),
+      createUser({ username: "curly", password: "c_pw" }),
+      createProduct({ name: "foo" }),
+      createProduct({ name: "bar" }),
+      createProduct({ name: "bazz" }),
+      createProduct({ name: "quq" }),
+      createProduct({ name: "fip" }),
+    ]
+  );
 
   console.log(await fetchUsers());
   console.log(await fetchProducts());
 
-  console.log(await fetchFavorites(moe.id));
-  const favorite = await createFavorite({ user_id: moe.id, product_id: foo.id });
-  app.listen(port, ()=> console.log(`listening on port ${port}`));
+  app.listen(port, () => console.log(`listening on port ${port}`));
 };
 
 init();
-
